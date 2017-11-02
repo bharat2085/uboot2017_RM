27ec44f8803caf8d7bfb70eff1772d1b966f51c4
bharat bhushan <bharatbhushan.basindia@gmail.com>
2017-11-02 16:07:59 +0530

    spl removed dram config comment

--- board/freescale/p1020_wlan/spl.c
+++ board/freescale/p1020_wlan/spl.c
@@ -129,7 +129,7 @@ void board_init_r(gd_t *gd, ulong dest_addr)
 #else
 	i2c_init(CONFIG_SYS_I2C_SPEED, CONFIG_SYS_I2C_SLAVE);
 #endif
-	puts("Setting dram...\n");
+	
 	gd->ram_size = initdram(0);
 #ifdef CONFIG_SPL_NAND_BOOT
 	puts("Tertiary program loader running in sram...");
